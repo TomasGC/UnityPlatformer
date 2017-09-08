db43a612e036550c803d122a6defdebd030c08e3
Tomas Gomes Cardoso <30507525+TomasGC@users.noreply.github.com>
2017-09-08 13:14:11 +0300

    Initial commit

---
+++ Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour{
+
+	// Use this for initialization
+	void Start(){
+    _player = gameObject.GetComponentInParent<Player>();
+	}
+	
+	// Called when the player touch the ground.
+  void OnTriggerEnter2D(Collider2D collider2D){
+    _player.SetGrounded(true);
+	}
+
+  // Called when the player leave the ground.
+  void OnTriggerExit2D(Collider2D collider2D){
+    _player.SetGrounded(false);
+  }
+
+  // Called while the player stays on the ground.
+  void OnTriggerStay2D(Collider2D collider2D){
+    _player.SetGrounded(true);
+  }
+
+  private Player _player;
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+  
+  public Player(){
+    _speed = 50f;
+    _jump = 150f;
+    _speedMax = 3f;
+  }
+
+  // Use this for initialization.
+	void Start () {
+    _rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+    _animator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame.
+	void Update() {
+    _animator.SetBool("Grounded", _grounded);
+    _animator.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+    /*
+    // To turn the player to the left or right depending the direction.
+    if(Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f){
+      Vector3 scale = transform.localScale;
+      scale.x *= -1;
+      transform.localScale = scale;
+    }
+    */
+    // To turn the player to the left.
+    if(Input.GetAxis("Horizontal") < -0.1f){
+      transform.localScale = new Vector3(-1, 1, 1);
+    }
+
+    // To turn the player to the right.
+    if(Input.GetAxis("Horizontal") > 0.1f){
+      transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    // To allow the player to jump.
+    if(Input.GetButtonDown("Jump") && _grounded){
+      _rigidBody2D.AddForce(Vector2.up * _jump);
+    }
+	}
+
+  // Update every fixed framerate frame.
+  void FixedUpdate() {
+    // Get the direction on the horizontal axis (right or left).
+    float direction = Input.GetAxis("Horizontal");
+
+    // Moving the player.
+    _rigidBody2D.AddForce((Vector2.right * _speed) * direction);
+
+    // Speed limitation.
+    if(_rigidBody2D.velocity.x > _speedMax){
+      _rigidBody2D.velocity = new Vector2(_speedMax, _rigidBody2D.velocity.y);
+    }
+    if(_rigidBody2D.velocity.x < -_speedMax) {
+      _rigidBody2D.velocity  = new Vector2(-_speedMax, _rigidBody2D.velocity.y);
+    }
+
+  }
+
+  public void SetGrounded(bool grounded){
+    _grounded = grounded;
+  }
+
+  // Public attributes.
+  public float _speed;
+  public float _jump;
+
+  // Private attributes.
+  private float _speedMax;
+  private bool _grounded;
+  private Rigidbody2D _rigidBody2D;
+  private Animator _animator;
+}
