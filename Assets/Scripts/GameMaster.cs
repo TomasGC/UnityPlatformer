a5ab760ada178d2a010c7051d77d1edf9a50a2d3
Tomas Gomes Cardoso <30507525+TomasGC@users.noreply.github.com>
2017-09-10 16:05:16 +0300

    Add Coin and fixes

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour {
+
+	// Use this for initialization.
+	void Start(){
+    _nbCoins = 0;
+	}
+	
+	// Update is called once per frame.
+	void Update(){
+    _textCoins.text = "Wallet: " + _nbCoins + " coins";
+	}
+
+  // To increase the number of coins the player have by coinsWin.
+  public void IncreaseNbCoins(int coinsWin){
+    _nbCoins += coinsWin;
+  }
+
+  // To decrease the number of coins the player have by coinsLost.
+  public void DecreaseNbCoins(int coinsLost){
+    _nbCoins -= coinsLost;
+  }
+
+  // Attributes.
+  private int _nbCoins;
+  public Text _textCoins;
+}
